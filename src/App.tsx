8cb9270f673fe754c95521bb78a76b48b8887020
Chas <Chas@chas.com>
2026-03-02 07:38:15 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Basic } from './components/Basic';
 //import { UseZod } from './components/UseZod';
 //import { TabsDemo } from './components/TabsDemo';
+import { Typography }from '@mui/material'
 
 function App() {
   
   return (
     <>
+      <Typography variant="h6" sx={{ m:3}}>Examples of individual React MUI tags can be found in the react-mui-vite-typescript repo</Typography>
        <Basic /> 
      {/*  < UseZod /> */}
       {/* < TabsDemo /> */}
